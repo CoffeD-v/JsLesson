641988ad99c84e7b422e497af315a2e82b3c65c3
Luiz Felipe Warmling Amadeu <85834483+Amad3eu@users.noreply.github.com>
2021-12-01 19:29:40 -0300

    Add files via upload

---
+++ step1/init.js
@@ -0,0 +1,22 @@
+// declaração de função
+
+function falaOi() {
+    console.log('ola')
+}
+
+falaoi();
+
+// first-class objects (Objetos de primeira classe)
+// Function expression
+const souUmDado = function() {
+    console.log('Sou um dado');
+};
+
+souUmDado();
+
+// Arrow function
+
+const funcaoArrow = () => {
+    console.log('Sou uma funcao arrow');
+}
+funcaoArrow();
