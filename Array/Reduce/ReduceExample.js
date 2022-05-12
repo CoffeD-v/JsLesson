08f0489f41c08c63f4246aeb172a3a04268127f7
Luiz Felipe Warmling Amadeu <85834483+Amad3eu@users.noreply.github.com>
2022-05-12 09:07:48 -0300

    updating file with error

--- Array/Reduce/ReduceExample.js
+++ Array/Reduce/ReduceExample.js
@@ -1,21 +1,22 @@
 // Some todos os numeros (reduce)
 // Retorne um array com os pares(filter)
 // Retorne um array com o dobro dos valores(map)
-//const numeros = [5,50,80,1,2,3,5,8,7,11,15,22,27]
-///const total = numeros.reduce(function (acumulador,valor) {
-//    acumulador.push(valor*2)
-    return acumulador;
-//},[])
 
-//console.log(total)
+const numeros = [5,50,80,1,2,3,5,8,7,11,15,22,27]
+const total = numeros.reduce(function (acumulador,valor) {
+   acumulador.push(valor*2)
+    return acumulador;
+},[])
+
+console.log(total)
 
 const pessoas = [
-    {nome: 'Rennyson', idade: 19},
+    {nome: 'Luiz', idade: 19},
     {nome: 'Maria', idade: 10},
-    {nome: 'Fernada', idade: 34},
-    {nome: 'Jose', idade: 76},
-    {nome: 'Joao', idade: 54},
-    {nome: 'Wallace', idade: 8},
+    {nome: 'Ronaldo', idade: 34},
+    {nome: 'Joseph', idade: 76},
+    {nome: 'Joao Carlos', idade: 54},
+    {nome: 'Wagner', idade: 8},
 ];
 
 const velha = pessoas.reduce(function(acumulador,valor) {
