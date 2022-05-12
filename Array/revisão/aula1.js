5dbfa4868233c4d0f21b7017d92f3e318041980c
Luiz Felipe Warmling Amadeu <85834483+Amad3eu@users.noreply.github.com>
2022-05-12 09:24:53 -0300

    Delete Array/revisão directory

--- Array/revisão/aula1.js
+++
@@ -1,8 +0,0 @@
-// Valor por referência
-//const nomes = ['Rennyson', 'Cavalcante', 'Soares','Teste1','Teste2']
-//const novo = nomes.slice(1,3)
-//console.log(novo)
-
-const nome = 'Luiz Felipe'
-const nomes = nome.split(' ')
-console.log(nomes)
