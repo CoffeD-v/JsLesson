c281ef44ef37497cf4482bf653cbfe35c9e43ec6
Luiz Felipe Warmling Amadeu <85834483+Amad3eu@users.noreply.github.com>
2022-05-18 09:36:55 -0300

    Update FilterExample.js

--- Array/Step01/Filter/FilterExample.js
+++ Array/Step01/Filter/FilterExample.js
@@ -2,27 +2,29 @@
 
 //Retorne os números maiores que 10
 
-//const numeros = [5,50,80,1,2,3,4,5,8,9,7,11,15,22,27]
+const numeros = [5,50,80,1,2,3,4,5,8,9,7,11,15,22,27]
 
-//const callFilter = numeros.filter(valor => valor > 10);
-
-
-///console.log(`os número maiores que 10 são ${callFilter}`)
+const callFilter = numeros.filter(valor => valor > 10);
 
 
+console.log(`os número maiores que 10 são ${callFilter}`)
 
-// Retorne as pessoas que tem o nome com 5 letras ou mais
-// Retorne as tem com mais de 50 anos 
-// retorne as pessoas cujo o nome termina com a letra a
+
+
+
+// Retorne as pessoas cujo o nome termina com a letra a
+// Retorne as pessoas cujo o nome começa com a letra A
 
 const pessoas = [
     {nome: 'Luiz', idade: 19},
     {nome: 'João', idade: 10},
-    {nome: 'Felipe', idade: 34},
-    {nome: 'Rodrigo', idade: 76},
-    {nome: 'Jonas', idade: 54},
-    {nome: 'Wallace', idade: 8},
+    {nome: 'Arnaldo', idade: 34},
+    {nome: 'Maria', idade: 76},
+    {nome: 'Ana', idade: 54},
+    {nome: 'Fernanda', idade: 8},
 ];
 
 const nomeA = pessoas.filter(obj => obj.nome.toLocaleLowerCase().endsWith('a'));
+const nomeB = pessoas.filter(obj => obj.nome.toLocaleUpperCase().startsWith('A'));
 console.log(nomeA);
+console.log(nomeB);
